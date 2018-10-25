5aa036e72e4f77b2eb70076c63cad96330c626dd
mooman219 <mooman219@gmail.com>
2018-10-24 22:18:55 -0700

    We don't need new_boxed_slice

--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -1,11 +1,3 @@
 pub mod benching;
 pub mod init;
 pub mod slotmap;
-
-pub fn new_boxed_slice<T>(len: usize) -> Box<[T]> {
-    let mut items: Vec<T> = Vec::<T>::with_capacity(len);
-    unsafe {
-        items.set_len(len);
-    }
-    items.into_boxed_slice()
-}
