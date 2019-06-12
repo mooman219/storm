1148a01dd81a2b27d434b4060278faf14c332a2a
mooman219 <mooman219@gmail.com>
2019-06-12 01:50:53 -0700

    target_os = macos

--- src/render/gl/raw/mod.rs
+++ src/render/gl/raw/mod.rs
@@ -21,16 +21,16 @@ pub use crate::render::gl::raw::vertex::*;
 /// `x, y` - Specify the lower left corner of the viewport rectangle, in pixels. The initial value
 /// is (0,0). `width, height` - Specify the width and height of the viewport. When a GL context is
 /// first attached to a window, width and height are set to the dimensions of that window.
-#[cfg(not(macos))]
 #[inline]
+#[cfg(not(target_os = "macos"))]
 pub fn viewport(x: i32, y: i32, width: i32, height: i32) {
     unsafe {
         gl::Viewport(x, y, width, height);
     }
 }
 
-#[cfg(macos)]
 #[inline]
+#[cfg(target_os = "macos")]
 pub fn viewport(_x: i32, _y: i32, _width: i32, _height: i32) {}
 
 #[inline(always)]
