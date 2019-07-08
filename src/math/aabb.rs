c0d4082302f2cdf25fd983f8ee143dd95547731e
mooman219 <mooman219@gmail.com>
2019-07-07 23:50:47 -0700

    jk

--- src/math/aabb.rs
+++ src/math/aabb.rs
@@ -120,8 +120,6 @@ impl AABB2D {
 mod tests {
     #![allow(unused_imports)]
     use super::*;
-    use test::black_box;
-    use test::Bencher;
 
     #[test]
     fn slide() {
@@ -139,30 +137,5 @@ mod tests {
             aabb.slide(&mot, &v);
             assert_eq!(aabb, AABB2D::new(1f32, 1f32, 2f32, 2f32));
         }
-    }
-
-    // ////////////////////////////////////////////////////////////////////////////
-    // Benches
-    // ////////////////////////////////////////////////////////////////////////////
-
-    #[bench]
-    fn bench_slide(b: &mut Bencher) {
-        let v = black_box(vec![
-            AABB2D::new(2f32, 0f32, 3f32, 1f32),
-            AABB2D::new(0f32, 1f32, 1f32, 2f32),
-            AABB2D::new(3f32, 1f32, 4f32, 2f32),
-            AABB2D::new(1f32, 2f32, 2f32, 3f32),
-            AABB2D::new(2f32, 0f32, 3f32, 1f32),
-            AABB2D::new(0f32, 1f32, 1f32, 2f32),
-            AABB2D::new(3f32, 1f32, 4f32, 2f32),
-            AABB2D::new(1f32, 2f32, 2f32, 3f32),
-        ]);
-        let mota = black_box(Vector2::new(2f32, 0f32));
-        let motb = black_box(Vector2::new(-4f32, 1f32));
-        b.iter(|| {
-            let mut aabb = black_box(AABB2D::new(0f32, 0f32, 1f32, 1f32));
-            aabb.slide(&mota, &v);
-            aabb.slide(&motb, &v);
-        });
     }
 }
