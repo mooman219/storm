92da371ac7e5b5a96761b324f98e1ba41b3ee5fd
marsmute <marsmute@protonmail.com>
2019-09-13 23:15:18 -0700

    Fixed commit characters that I missed

--- examples/tetris_game/mod.rs
+++ examples/tetris_game/mod.rs
@@ -1,7 +1,6 @@
 mod audio_engine;
 mod tetris_cluster;
 mod tetris_state;
-<<<<<<< HEAD
 mod ui_engine;
 mod button;
 
@@ -10,9 +9,3 @@ pub use tetris_cluster::*;
 pub use tetris_state::*;
 pub use ui_engine::*;
 pub use button::*;
-=======
-
-pub use audio_engine::*;
-pub use tetris_cluster::*;
-pub use tetris_state::*;
->>>>>>> 26585cc599c4e63db10bc46e9fe721778d0fd423
